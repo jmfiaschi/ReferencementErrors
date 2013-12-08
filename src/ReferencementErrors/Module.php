1c95ec8983c958e2a33976e140557864d8064108
jmfiaschi <jm.fiaschi@gmail.com>
2013-12-08 20:03:00 +0100

    initialisation du projet

---
+++ src/ReferencementErrors/Module.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace ReferencementErrors;
+
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\Mvc\MvcEvent;
+
+class Module
+    implements BootstrapListenerInterface,
+               ConfigProviderInterface,
+               ServiceProviderInterface,
+               DependencyIndicatorInterface
+{
+
+    /**
+     * Listen to the bootstrap event
+     *
+     * @param MvcEvent|EventInterface $e
+     * @return array
+     */
+    public function onBootstrap(EventInterface $e)
+    {
+
+        $application    = $e->getApplication();
+        $serviceManager = $application->getServiceManager();
+    }
+
+
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    /**
+     *
+     * Set autoloader config for RbacUserDoctrineOrm module
+     *
+     * @return array\Traversable
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+    
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to
+     * seed such an object.
+     *
+     * @return array|\Zend\ServiceManager\Config
+     */
+    public function getServiceConfig()
+    {
+        /*return array(
+            'factories' => array(
+                'RbacUserDoctrineOrmRoleMapper' => function ($sm) {
+                    return new Mapper\Role(
+                        $sm->get('zfcuser_doctrine_em'),
+                        $sm->get('RbacUserDoctrineOrmRoleMapperOptions')
+                    );
+                },
+                'RbacUserDoctrineOrmRoleMapperOptions' => function ($sm) {
+                    $config = $sm->get('Configuration');
+                    return new Options\RoleMapperOptions(
+                        (isset($config['rbac-user-doctrine-orm']['mapper']['role'])
+                            ? $config['rbac-user-doctrine-orm']['mapper']['role']
+                            : array())
+                    );
+                },
+                'Zend\Authentication\AuthenticationService' => function ($serviceManager) {
+                	return $serviceManager->get('doctrine.authenticationservice.orm_default');
+                }
+            ),
+        );*/
+    }
+
+    /**
+     * Expected to return an array of modules on which the current one depends on
+     *
+     * @return array
+     */
+    /*public function getModuleDependencies()
+    {
+        return array(
+            'DoctrineModule', 'DoctrineORMModule', 'ZfcBase',
+            'ZfcUser', 'ZfcUserDoctrineORM', 'ZfcRbac'
+        );
+    }*/
+}
